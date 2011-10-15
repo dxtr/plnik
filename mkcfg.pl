6e202b353460bae98643e9eabb7ab1e6b79b7378
Kim Lidström <kim.lidas+github@gmail.com>
2011-10-15 08:33:08 +0200

    Just updated the config generation so it actually works.

--- mkcfg.pl
+++ mkcfg.pl
@@ -2,10 +2,10 @@ use strict;
 use warnings;
 use JSON;
 
-my %settings = ('server' => {'FreeNode' => ['irc.undernet.org']}, 'port' => 6667,
+my %settings = ('server' => ['irc.undernet.org'], 'port' => 6667,
 	'password' => '', 'ssl' => 0, 'nickname' => 'plnik', 'username' => 'plnik',
-	'realname' => 'plnik', 'bind' => '0.0.0.0', 'owner' => 'dxtr!*@unaffiliated/dxtr',
-	'modules' => ['Autojoin', 'Markov']);
+	'realname' => 'plnik', 'bind' => '0.0.0.0', 'channels' => [["#plnik", ""]],
+    'owner' => 'dxtr!*@unaffiliated/dxtr', 'modules' => ['Autojoin', 'Markov']);
 my $sref = \%settings;
 my $json = JSON->new->pretty([1]);
 
